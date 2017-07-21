da6eac761ad3803f6577ed241c25cf10d8e09fe9
Adhy <adhywiranatap@gmail.com>
2017-07-21 23:26:19 +0700

    add font face observer

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import glamorous from 'glamorous';
+import FontFaceObserver from 'fontfaceobserver';
 
 const AppContainer = glamorous.div({
+  fontFamily: 'Open Sans'
+});
+
+const font = new FontFaceObserver('Open Sans');
+
+font.load().then(() => {
+  // console.log('Open Sans has loaded.');
+}).catch(err => {
+  console.error(err);
 });
 
 export default class extends Component {
