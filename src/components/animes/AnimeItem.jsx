83f22e9278b4f644d407369125c683ff3784fa48
Adhy <adhywiranatap@gmail.com>
2017-07-22 00:59:55 +0700

    add anime item component

---
+++ src/components/animes/AnimeItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import glamorous from 'glamorous';
+
+import { PlainLink, Link } from '../core/Link';
+
+const ItemWrapper = glamorous.div({
+  margin: '10px 1%',
+  width: '48%',
+  height: 'auto',
+  borderRadius: 3,
+  overflow: 'hidden',
+  backgroundColor: '#F5F5F5',
+});
+
+const ItemCover = glamorous.img({
+  width: '100%',
+});
+
+
+export default () => (
+  <ItemWrapper>
+    <PlainLink to={'/'}>
+      <ItemCover
+        src={'https://media.kitsu.io/anime/poster_images/12/small.jpg?1490541434'}
+        alt={'one piece'}
+      />
+    </PlainLink>
+    <Link to={'/'}>One Piece</Link>
+  </ItemWrapper>
+);
--- src/components/animes/AnimeList.jsx
+++ src/components/animes/AnimeList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import AnimeItem from './AnimeItem';
+
 export default () => (
   <div>
     list...
+    <div style={{ padding: 5, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <AnimeItem />
+      <AnimeItem />
+      <AnimeItem />
+      <AnimeItem />
+    </div>
   </div>
 );
