be321bcdaeca89933d6d7a20ec53c7439fa3d957
Adhy <adhywiranatap@gmail.com>
2017-07-26 06:56:49 +0700

    add anime reducer with initial state

---
+++ src/reducers/animeReducer.js
@@ -0,0 +1,11 @@
+// @flow
+const initialState = {
+  animesData: [],
+  isFetching: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import animeReducer from './animeReducer';
+
 export default () => combineReducers({
   router: routerReducer,
+  animes: animeReducer,
 });
 
