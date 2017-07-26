9738ad2313cb26fd27525552be1a0fc8ffd0f017
Adhy <adhywiranatap@gmail.com>
2017-07-26 20:35:56 +0700

    add react-snapshot to add SSR capability

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // @flow
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
