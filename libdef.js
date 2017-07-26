e81f656aff8b7eca39259a7cd5d65b9beab4def2
Adhy <adhywiranatap@gmail.com>
2017-07-26 11:00:11 +0700

    add libdef for flow third party modules

---
+++ libdef.js
@@ -0,0 +1,10 @@
+declare module 'react-helmet' { declare var exports: any };
+declare module 'react-router-dom' { declare var exports: any };
+declare module 'react-router-redux' { declare var exports: any };
+declare module 'redux' { declare var exports: any };
+declare module 'react-redux' { declare var exports: any };
+declare module 'immutable' { declare var exports: any };
+declare module 'history/createBrowserHistory' { declare var exports: any };
+declare module 'glamor' { declare var exports: any };
+declare module 'glamorous' { declare var exports: any };
+declare module 'fontfaceobserver' { declare var exports: any };
