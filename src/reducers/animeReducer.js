7c2bd41547329002df225cab802dff433d3d3f4e
Adhy <adhywiranatap@gmail.com>
2017-07-26 07:03:40 +0700

    add flow type for anime Reducer

--- src/reducers/animeReducer.js
+++ src/reducers/animeReducer.js
@@ -1,10 +1,20 @@
 // @flow
+
+type animeStateType = {
+  animesData: number[],
+  isFetching: boolean,
+};
+
+type actionCreatorType = {
+  type: string,
+};
+
 const initialState = {
   animesData: [],
   isFetching: false,
 };
 
-export default (state = initialState, action) => {
+export default (state: ?animeStateType = initialState, action: actionCreatorType) => {
   switch (action.type) {
     default: return state;
   }
