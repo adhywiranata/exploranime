43bfdaaeba7b75aa54e23703e86d14381f110761
Adhy <adhywiranatap@gmail.com>
2017-07-27 19:30:08 +0700

    Update anime action creators

--- src/actions/animeActions.js
+++ src/actions/animeActions.js
@@ -1,5 +1,13 @@
-import { FETCH_ANIMES } from './constants';
+import { FETCH_ANIMES, FETCH_ANIMES_LOADING, FETCH_ANIMES_SUCCESS } from './constants';
 
 export const fetchAnimes = () => ({
   type: FETCH_ANIMES,
+});
+
+export const fetchAnimesLoading = () => ({
+  type: FETCH_ANIMES_LOADING,
+});
+
+export const fetchAnimesSuccess = () => ({
+  type: FETCH_ANIMES_SUCCESS,
 });
