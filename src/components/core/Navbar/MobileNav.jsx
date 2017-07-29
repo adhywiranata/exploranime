fa16c3488a87cfda62856aa2643e1c4966f7c36d
adhywiranata <adhywiranatap@gmail.com>
2017-07-29 22:09:19 +0700

    add mobile nav styling and menu

--- src/components/core/Navbar/MobileNav.jsx
+++ src/components/core/Navbar/MobileNav.jsx
@@ -1,10 +1,26 @@
 // @flow
 import React from 'react';
 
-import { MobileNavOverlay } from './styles';
+import {
+  MobileNavOverlay as Overlay,
+  MobileNavContainer as Container,
+  MobileNavHeader as Header,
+  MobileNavList as Ul,
+  MobileNavListItem as Li,
+} from './styles';
 
 export default () => (
   <div>
-    <MobileNavOverlay />
+    <Overlay />
+    <Container>
+      <Header>
+        menu
+      </Header>
+      <Ul>
+        <Li>Home</Li>
+        <Li>About</Li>
+        <Li>Login</Li>
+      </Ul>
+    </Container>
   </div>
 );
--- src/components/core/Navbar/styles.js
+++ src/components/core/Navbar/styles.js
@@ -7,3 +7,29 @@ export const MobileNavOverlay = glamorous.div({
   height: '100vh',
   zIndex: 1,
 });
+
+export const MobileNavContainer = glamorous.div({
+  backgroundColor: '#FFFFFF',
+  position: 'fixed',
+  width: '80vw',
+  height: '100vh',
+  zIndex: 3,
+  boxShadow: '3px 1px 10px 1px rgba(0,0,0,0.1)',
+});
+
+export const MobileNavHeader = glamorous.header({
+  background: 'linear-gradient(to right, #3a6073, #3a7bd5)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  padding: 30,
+  boxSizing: 'border-box',
+  color: 'white',
+});
+
+export const MobileNavList = glamorous.ul({
+  padding: 0,
+});
+
+export const MobileNavListItem = glamorous.li({
+  padding: '20px 20px',
+  borderBottom: '1px solid rgba(0,0,0,0.1)',
+  fontSize: '0.8rem',
+});
