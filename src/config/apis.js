35b5af6cb9ced5240637d7889fd640541fc3b011
adhywiranata <adhywiranatap@gmail.com>
2017-07-29 22:41:00 +0700

    fix windows LF to CRLF

--- src/config/apis.js
+++ src/config/apis.js
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-export const fetchKitsuAnimesPaginated = (offset) => {
-  return axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${offset}&sort=-averageRating`);
-}
+export const fetchKitsuAnimesPaginated = offset => axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${offset}&sort=-averageRating`);
