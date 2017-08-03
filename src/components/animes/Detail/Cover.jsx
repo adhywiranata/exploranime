3e2914dd3de42833fcdc600f825b8d550bb38892
Adhy <adhywiranatap@gmail.com>
2017-08-04 06:02:43 +0700

    refactor anime details and separate details content and cover component

---
+++ src/components/animes/Detail/Cover.jsx
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+
+import { H2 } from '../../core/TextHeadings';
+
+export default () => (
+  <div style={{
+    background: 'url("https://media.kitsu.io/anime/cover_images/10740/original.jpg?1445804501")',
+    height: 100,
+    width: '100%',
+    padding: '50px 0',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+    position: 'relative',
+  }}>
+    <div style={{
+      background: 'rgba(0,0,0,0.5)',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      padding: 30,
+      boxSizing: 'border-box',
+      top: 0,
+    }}>
+      <H2 style={{ color: 'white', position: 'absolute', bottom: 0 }}>
+        Ano hi Mita Hana No Namae Wo Bokutachi Wa Shiranai
+      </H2>
+    </div>
+  </div>
+);
--- src/components/animes/DetailSection.jsx
+++ src/components/animes/DetailSection.jsx
@@ -1,34 +1,12 @@
 // @flow
 import React from 'react';
 
-import Detail from './Detail';
-import { H2 } from '../core/TextHeadings';
+import Cover from './Detail/Cover';
+import Detail from './Detail/Content';
 
 export default (): React$Element<any> => (
   <div style={{ padding: 0, display: 'flex', flexDirection: 'column' }}>
-    <div style={{
-      background: 'url("https://media.kitsu.io/anime/cover_images/10740/original.jpg?1445804501")',
-      height: 100,
-      width: '100%',
-      padding: '50px 0',
-      backgroundAttachment: 'fixed',
-      backgroundSize: 'cover',
-      position: 'relative',
-    }}>
-      <div style={{
-        background: 'rgba(0,0,0,0.5)',
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        padding: 30,
-        boxSizing: 'border-box',
-        top: 0,
-      }}>
-        <H2 style={{ color: 'white', position: 'absolute', bottom: 0 }}>
-          Ano hi Mita Hana No Namae Wo Bokutachi Wa Shiranai
-        </H2>
-      </div>
-    </div>
+    <Cover />
     <Detail />
   </div>
 );
