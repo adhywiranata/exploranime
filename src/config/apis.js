22fb9a2ae27a4fff5e4ad32336ffe05b36bd42ba
Adhy <adhywiranatap@gmail.com>
2017-08-04 05:39:53 +0700

    add fetch anime detail API configuration

--- src/config/apis.js
+++ src/config/apis.js
@@ -1,3 +1,8 @@
 import axios from 'axios';
 
-export const fetchKitsuAnimesPaginated = offset => axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${offset}&sort=-averageRating`);
+export const fetchKitsuAnimesPaginated = offset => (
+  axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${offset}&sort=-averageRating`)
+);
+export const fetchKitsuAnimeDetails = id => (
+  axios.get(`https://kitsu.io/api/edge/anime?filter[id]=${id}`)
+);
