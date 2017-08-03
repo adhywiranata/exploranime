a748fa253524c770b315e0cb882835b3535006a9
Adhy <adhywiranatap@gmail.com>
2017-08-04 05:45:22 +0700

    add action creators for anime details fetching

--- src/actions/animeActions.js
+++ src/actions/animeActions.js
@@ -1,5 +1,15 @@
-import { FETCH_ANIMES, FETCH_ANIMES_LOADING, FETCH_ANIMES_SUCCESS, FETCH_ANIMES_FAILURE } from './constants';
+import {
+  FETCH_ANIMES,
+  FETCH_ANIMES_LOADING,
+  FETCH_ANIMES_SUCCESS,
+  FETCH_ANIMES_FAILURE,
+  FETCH_ANIME_DETAILS,
+  FETCH_ANIME_DETAILS_LOADING,
+  FETCH_ANIME_DETAILS_SUCCESS,
+  FETCH_ANIME_DETAILS_FAILURE,
+} from './constants';
 
+// Action Creators for Fetch Animes List
 export const fetchAnimes = offset => ({
   type: FETCH_ANIMES,
   payload: offset,
@@ -16,4 +26,21 @@ export const fetchAnimesSuccess = animesData => ({
 
 export const fetchAnimesFailure = () => ({
   type: FETCH_ANIMES_FAILURE,
+});
+
+export const fetchAnimeDetails = () => ({
+  type: FETCH_ANIME_DETAILS,
+});
+
+// Action Creators for Fetching Anime Details
+export const fetchAnimeDetailsLoading = () => ({
+  type: FETCH_ANIME_DETAILS_LOADING,
+});
+
+export const fetchAnimeDetailsSuccess = () => ({
+  type: FETCH_ANIME_DETAILS_SUCCESS,
+});
+
+export const fetchAnimeDetailsFailure = () => ({
+  type: FETCH_ANIME_DETAILS_FAILURE,
 });
