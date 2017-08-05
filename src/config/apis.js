56258955e242f33cca3be36507d628fd748f40a5
Adhy <adhywiranatap@gmail.com>
2017-08-05 19:10:28 +0700

    add fetch search animes

--- src/config/apis.js
+++ src/config/apis.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 export const fetchKitsuAnimesPaginated = offset => (
   axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${offset}&sort=-averageRating`)
 );
+
+export const fetchKitsuAnimeSearch = keyword => (
+  axios.get(`https://kitsu.io/api/edge/anime?filter[text]=cowboy%20bebop`);
+);
+
 export const fetchKitsuAnimeDetails = id => (
   axios.get(`https://kitsu.io/api/edge/anime?filter[id]=${id}`)
 );
