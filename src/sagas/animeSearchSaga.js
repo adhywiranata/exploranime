c1395eac5fef933398d8eb40bceff0db277327c9
Adhy <adhywiranatap@gmail.com>
2017-08-05 21:07:43 +0700

    add anime search redux saga

---
+++ src/sagas/animeSearchSaga.js
@@ -0,0 +1,28 @@
+import { put, call, takeEvery } from 'redux-saga/effects';
+
+import {
+  setAnimeSearchTerm as setAnimeSearchTermActionCreator,
+  fetchAnimesByKeywordSuccess as fetchAnimesByKeywordSuccessActionCreator,
+  fetchAnimesByKeywordFailure as fetchAnimesByKeywordFailureActionCreator,
+} from '../actions/animeActions';
+
+import {
+  fetchKitsuAnimeSearch,
+} from '../config/apis';
+
+import { FETCH_ANIMES_BY_KEYWORD } from '../actions/constants';
+
+export function* fetchAnimesByKeyword(action) {
+  yield put(setAnimeSearchTermActionCreator(action.payload));
+  const animesData = yield call(fetchKitsuAnimeSearch, action.payload);
+  try {
+    // TODO handle if no anime id found!
+    yield put(fetchAnimesByKeywordSuccessActionCreator(animesData.data.data[0]));
+  } catch (e) {
+    yield put(fetchAnimesByKeywordFailureActionCreator());
+  }
+}
+
+export function* watchFetchAnimeSearchByKeyword() {
+  yield takeEvery(FETCH_ANIMES_BY_KEYWORD, fetchAnimesByKeyword);
+}
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,16 +1,13 @@
 import { all } from 'redux-saga/effects';
 
-import {
-  watchFetchAnimes,
-} from './animeSaga';
-
-import {
-  watchFetchAnimeDetails,
-} from './animeDetailsSaga';
+import { watchFetchAnimes } from './animeSaga';
+import { watchFetchAnimeDetails } from './animeDetailsSaga';
+import { watchFetchAnimeSearchByKeyword } from './animeSearchSaga';
 
 export default function* rootSaga() {
   yield all([
     watchFetchAnimes(),
     watchFetchAnimeDetails(),
+    watchFetchAnimeSearchByKeyword(),
   ]);
 }
