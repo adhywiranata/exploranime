119d7aea7e91da544319ec97cf10492664b6c6d9
Adhy <adhywiranatap@gmail.com>
2017-08-13 19:03:05 +0700

    add anime characters detail fetching action creators

--- src/actions/animeCharacterActions.js
+++ src/actions/animeCharacterActions.js
@@ -4,6 +4,9 @@ import {
   FETCH_ANIMES_CHARACTERS_BY_ANIME_SUCCESS,
   FETCH_ANIMES_CHARACTERS_BY_ANIME_FAILURE,
   FETCH_ANIME_CHARACTER_DETAILS,
+  FETCH_ANIME_CHARACTER_DETAILS_LOADING,
+  FETCH_ANIME_CHARACTER_DETAILS_SUCCESS,
+  FETCH_ANIME_CHARACTER_DETAILS_FAILURE,
 } from './constants';
 
 export const fetchAnimesCharacters = animeId => ({
@@ -27,4 +30,17 @@ export const fetchAnimesCharactersFailure = () => ({
 export const fetchAnimeCharacterDetails = characterId => ({
   type: FETCH_ANIME_CHARACTER_DETAILS,
   payload: characterId,
+});
+
+export const fetchAnimeCharacterDetailsLoading = () => ({
+  type: FETCH_ANIME_CHARACTER_DETAILS_LOADING,
+});
+
+export const fetchAnimeCharacterDetailsSuccess = (character) => ({
+  type: FETCH_ANIME_CHARACTER_DETAILS_SUCCESS,
+  payload: character,
+});
+
+export const fetchAnimeCharacterDetailsFailure = () => ({
+  type: FETCH_ANIME_CHARACTER_DETAILS_FAILURE,
 });
